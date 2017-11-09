cbc3bb5a33bc0f4b8cacfd296b83c8e2016f88f2
Oren Cohen <oren002@gmail.com>
2017-11-09 20:20:33 +0200

    Update JenkinsfileTest.groovy

--- JenkinsfileTest.groovy
+++ JenkinsfileTest.groovy
@@ -2,7 +2,7 @@ try {
     node('master') {
         timestamps {
             stage('Publish Blueprint'){
-                sh "curl -X POST --header 'Content-Type: application/json-patch+json' --header 'Accept: application/json' -d '{ \"blueprint_name\": \"fasty-k8s\" }' 'http://cs18-api.sabox.com:5050/api/catalog'"
+                sh "curl -X POST --header 'Content-Type: application/json-patch+json' --header 'Accept: application/json' -d '{ \"blueprint_name\": \"fasty-k8s\" }' 'http://cs18-api.sandbox.com:5050/api/catalog'"
             }
             stage('Integration Test') {
                 def release = [:]
