c3f235c29a5795d7b70b4affc08fa6d122b64c79
Tomer Cohen <tomer.c@quali.com>
2017-11-13 14:29:33 +0200

    added timeout

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,62 +9,64 @@ properties([buildDiscarder(logRotator(numToKeepStr: '100', artifactNumToKeepStr:
 
 try {
     node('gp1') {
-        timestamps {
-            stage('Init') {
-                node('master') {
-                    devops = load('/var/lib/jenkins/devopsLoader.groovy').loadDevops(devopsVersion)
-                }
-            }
-            stage('Checkout') {
-                devops.resetWorkspace(devopsVersion)
-                dir('cs18') {
-                    def scmVars = checkout scm
-                    devops.logalore(devopsVersion, scmVars)
-                    devops.grantFullPermissions('.')
-                    changeset = scmVars.GIT_COMMIT
-                }
-            }
-            
-            stage('Clean, Package & upload') {
-                dir('cs18') {
-                    devops.runSh('mvn -B -Dmaven.test.skip=true package') // TODO: add clean
-                    devops.grantFullPermissions("target") //somehow there is a problem with permissions!?
-                    dir('target'){
-                        echo "branch: '${env.BRANCH_NAME}'"
-                        devops.runSh("echo '${env.BRANCH_NAME}' > branch.txt")
-                        devops.runSh('ls')
-                        echo "${changeset}"
-                        devops.uploadArtifact("cs18.hpi")
-                        echo "uploadArtifact cs18.hpi"
-                        devops.uploadToS3("branch.txt", "ngdevbox/applications/jenkins/${changeset}")
-                        echo "uploadToS3 branch.txt"
-                        devops.uploadToS3("cs18.hpi", "ngdevbox/applications/jenkins/${changeset}")
-                        echo "uploadToS3 cs18.hpi"
+        timeout(time: 60, unit: 'MINUTES') {
+            timestamps {
+                stage('Init') {
+                    node('master') {
+                        devops = load('/var/lib/jenkins/devopsLoader.groovy').loadDevops(devopsVersion)
                     }
                 }
-            }
-            stage('Integration test'){
-                def release = [:]
-                release['jenkins'] = changeset
-                release['cs18-api'] = 'forFE'
-                release['cs18-account-ms'] = 'forFE'
-                release['cs18-db'] = 'forFE'
-                cs18.blueprint("n-ca-jenkins-aws", release).doInsideSandbox {
-                    echo "sanbox env: ${env.SANDBOX}"
-                    def sandbox = readJSON text: "${env.SANDBOX}"
-                    def url
-                    //start job named test1
-                    def jobName = "test1"
-                    for (application in sandbox.applications) {
-                        if (application["name"] == "jenkins") {
-                            url = application["shortcuts"][0]
-                            break
+                stage('Checkout') {
+                    devops.resetWorkspace(devopsVersion)
+                    dir('cs18') {
+                        def scmVars = checkout scm
+                        devops.logalore(devopsVersion, scmVars)
+                        devops.grantFullPermissions('.')
+                        changeset = scmVars.GIT_COMMIT
+                    }
+                }
+
+                stage('Clean, Package & upload') {
+                    dir('cs18') {
+                        devops.runSh('mvn -B -Dmaven.test.skip=true package') // TODO: add clean
+                        devops.grantFullPermissions("target") //somehow there is a problem with permissions!?
+                        dir('target') {
+                            echo "branch: '${env.BRANCH_NAME}'"
+                            devops.runSh("echo '${env.BRANCH_NAME}' > branch.txt")
+                            devops.runSh('ls')
+                            echo "${changeset}"
+                            devops.uploadArtifact("cs18.hpi")
+                            echo "uploadArtifact cs18.hpi"
+                            devops.uploadToS3("branch.txt", "ngdevbox/applications/jenkins/${changeset}")
+                            echo "uploadToS3 branch.txt"
+                            devops.uploadToS3("cs18.hpi", "ngdevbox/applications/jenkins/${changeset}")
+                            echo "uploadToS3 cs18.hpi"
                         }
                     }
-                    echo "url: ${url}"
-                    def innerLog =  devops.runJenkinsJob(jobName, url, true)
-                    writeFile file: 'innerLog.txt', text: innerLog
-                    devops.uploadArtifact("innerLog.txt")
+                }
+                stage('Integration test') {
+                    def release = [:]
+                    release['jenkins'] = changeset
+                    release['cs18-api'] = 'forFE'
+                    release['cs18-account-ms'] = 'forFE'
+                    release['cs18-db'] = 'forFE'
+                    cs18.blueprint("n-ca-jenkins-aws", release).doInsideSandbox {
+                        echo "sanbox env: ${env.SANDBOX}"
+                        def sandbox = readJSON text: "${env.SANDBOX}"
+                        def url
+                        //start job named test1
+                        def jobName = "test1"
+                        for (application in sandbox.applications) {
+                            if (application["name"] == "jenkins") {
+                                url = application["shortcuts"][0]
+                                break
+                            }
+                        }
+                        echo "url: ${url}"
+                        def innerLog = devops.runJenkinsJob(jobName, url, true)
+                        writeFile file: 'innerLog.txt', text: innerLog
+                        devops.uploadArtifact("innerLog.txt")
+                    }
                 }
             }
         }
