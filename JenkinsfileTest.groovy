9a8b5d172cc10da30e73f6b292cdd3d8756369bd
Tomer Cohen <tomer.c@quali.com>
2017-11-19 16:07:21 +0200

    testing the env

--- JenkinsfileTest.groovy
+++ JenkinsfileTest.groovy
@@ -1,18 +1,29 @@
 try {
     node('master') {
-        timestamps {
-            stage('Publish Blueprint'){
-                sh "curl -X POST --header 'Content-Type: application/json-patch+json' --header 'Accept: application/json' -d '{ \"blueprint_name\": \"fasty-k8s\" }' 'http://cs18-api.sandbox.com:5050/api/catalog'"
-            }
-            stage('Integration Test') {
-                def release = [:]
-                release["fasty"] = ""
-                cs18.blueprint("fasty-k8s", release).startSandbox()
-                writeFile file: 'sandbox_data.json', text: "${env.SANDBOX}"
+        timeout(time: 60, unit: 'MINUTES') {
+            timestamps {
+                stage('Publish Blueprint') {
+                    sh "curl -X POST --header 'Content-Type: application/json-patch+json' --header 'Accept: application/json' -d '{ \"blueprint_name\": \"fasty-k8s\" }' 'http://cs18-api.sandbox.com:5050/api/catalog'"
+                }
+                stage('Integration Test') {
+                    def release = [:]
+                    release["fasty"] = ""
+                    cs18.blueprint("fasty-k8s", release).doInsideSandbox() {
+                        echo "doInsideSandbox - from the enviroment param - env.SANDBOX: ${env.SANDBOX}"
+                    }
+
+                    try {
+                        def sandbox = cs18.blueprint("fasty-k8s", release).startSandbox()
+                        echo "startSandbox - from the enviroment param - env.SANDBOX: ${env.SANDBOX}"
+                    }
+                    finally {
+                        sandbox.end()
+                    }
+                }
             }
         }
     }
 }
-catch(Exception ex){
+catch (Exception ex) {
     throw ex
 }
