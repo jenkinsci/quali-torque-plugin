007ffe3660653655db74a33638e6e7871aadf770
Tomer Cohen <tomer.c@quali.com>
2018-02-12 10:24:31 +0200

    adding missing apps to release

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -55,9 +55,12 @@ try {
                     def release = [:]
 
                     release['jenkins'] = changeset
-                    release['cs18-api'] = 'forDexter'
-                    release['cs18-account-ms'] = 'forDexter'
-                    release['cs18-db'] = 'forDexter'
+                    release['cs18-api'] = "forDexter"
+                    release['cs18-account-ms'] = "forDexter"
+                    release['cs18-db'] = "forDexter"
+                    release['cs18-notifications-ms'] = "forDexter"
+                    release['cs18-rabbitmq'] = "forDexter"
+                    release['cs18-ui'] = "forDexter"
                     def sandbox = colony.blueprint("demo trial", "n-ca-jenkins-aws", "jenkinsAndCs18ForPlugin", release, 20).startSandbox() //{ sandbox ->
                         echo "sandbox env: " + sandbox.toString()
 
