eceadd78cfff1384ab87988057cb371d98635f52
Oleksandr Vasyliev <oleksandr.v@quali.com>
2022-08-31 14:41:14 +0200

    Changing constants

--- src/main/java/org/jenkinsci/plugins/torque/PluginConstants.java
+++ src/main/java/org/jenkinsci/plugins/torque/PluginConstants.java
@@ -4,14 +4,10 @@ package org.jenkinsci.plugins.torque;
  * Created by shay-k on 21/06/2017.
  */
 public class PluginConstants {
-    public static final String START_SANDBOX_FUNC_NAME ="startSandbox";
+    public static final String START_SANDBOX_FUNC_NAME ="startTorqueSandboxEnvironment";
 
-    public static final String END_SANDBOX_FUNC_NAME ="endSandbox";
+    public static final String END_SANDBOX_FUNC_NAME ="endTorqueSandboxEnvironment";
 
-    public static final String WITH_SANDBOX_FUNC_NAME="withSandbox";
-
-    public static final String WAIT_FOR_SANDBOX_FUNC_NAME ="waitForSandbox";
-
-    public static final String SANDBOX_ENVVAR = "SANDBOX";
+    public static final String WAIT_FOR_SANDBOX_FUNC_NAME ="waitForTorqueSandboxEnvironment";
 }
 
